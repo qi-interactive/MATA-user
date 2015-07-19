831806d9a445d06c7927145bc4534354099061a9
Michal Fiedorowicz <michal@qi-interactive.com>
2015-07-19 20:08:14 +0100

    Removed navigation required by matacms

--- Module.php
+++ Module.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /**
  * @link http://www.matacms.com/
  * @copyright Copyright (c) 2015 Qi Interactive Limited
@@ -87,25 +87,4 @@ class Module extends BaseModule
         'settings/<action:\w+>'       => 'settings/<action>'
     ];
 
-    public function getNavigation() {
-
-        return [
-            [
-                "label" => "Your profile",
-                "url" => "/mata-cms/user/settings",
-                "icon" => "/images/user-profile-account-default.svg"
-            ],
-            [
-                "label" => "Manage users",
-                "url" => "/mata-cms/user/admin/index",
-                "icon" => "/images/user-profile-account-default.svg"
-            ],
-            [
-                "label" => "Logout",
-                "url" => "/mata-cms/user/logout",
-                "icon" => "/images/logout.svg",
-                "class" => 'hard-link'
-            ]
-            ];
-    }
 }
